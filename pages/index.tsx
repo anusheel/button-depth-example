73300eead7ea385648d6eab90cf1d889b326501e
anusheel <anusheel@gmail.com>
2023-11-06 03:04:59 +0000

    Commit from Stub AI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import EmailInput from '../components/EmailInput';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <EmailInput />
     </main>
-  )
+  );
 }
