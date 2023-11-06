e1e929f1a53e869750a4d368a2a79ee208c2f6cf
anusheel <anusheel@gmail.com>
2023-11-06 03:06:30 +0000

    Commit from Stub AI

--- components/EmailInput.tsx
+++ components/EmailInput.tsx
@@ -11,7 +11,7 @@ const EmailInput: React.FC = () => {
         />
         <button 
           type="submit" 
-          className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="px-4 py-2 bg-gray-700 text-white rounded-md shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500"
         >
           Submit
         </button>
